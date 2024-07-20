07329bc35a8b6a58c277ee2469da9559d6d34045
Gabriel Gonzatti <109048186+GabrielGonzatti@users.noreply.github.com>
2024-07-20 16:25:06 -0300

    Atualizacao Java

---
+++ ExercíciosJava/src/exercicios.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class exercicios{
+    public static void main(String[] args) {
+    	int n1, n2;
+    	Scanner ler = new Scanner(System.in);
+        System.out.print("Informe um número por favor: \n");
+        n1 = ler.nextInt();
+        System.out.print("Informe o segundo número por favor: \n");
+        n2 = ler.nextInt();
+        int soma = n1 + n2;
+        double divisao = n1/n2;
+        int multi = n1*n2;
+        int sub = n1 - n2;
+        
+        System.out.println("A soma dos números: " + soma);
+        System.out.println("A subtração dos números: " + sub);
+        System.out.println("A divisão dos números: " + divisao);
+        System.out.println("A multiplicação dos números: " + multi);
+        
+    }
+}
